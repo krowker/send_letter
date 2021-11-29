8538e0252d600a26c3a623a62035dd9ac550b76f
krowker <krowker97@gmail.com>
2021-11-30 02:07:00 +0300

    add token service

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -1,19 +1,20 @@
 const UserModel = require('../models/user-model')
 const bcrypt = require('bcrypt')
+const ApiError = require('../exceptions/api-error')
 
 class UserService {
    async registration (email, password) {
       const condidate =  await UserModel.findOne({email})
       if (condidate) {
-
+         throw ApiError.BadRequest(`${email} уже зарегистрирован`)
       }
-
+      
    }
 
    async activate (activationLink) {
       const user = await UserModel.findOne({activationLink})
       if (!user) {
-         throw
+         throw ApiError.BadRequest('Неверная ссылка активации')
       }
       user.isActivated = true
       await user.save()

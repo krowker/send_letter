ed52b6971640555a81a0e0fde7fbbd1f7d088f31
krowker <krowker97@gmail.com>
2021-12-30 22:14:46 +0300

    add mui, 1st edit auth page

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,9 +1,66 @@
 import React from 'react'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import OutlinedInput from '@mui/material/OutlinedInput'
+import InputAdornment from '@mui/material/InputAdornment'
+import AccountCircle from '@mui/icons-material/AccountCircle'
+import { IconButton } from '@mui/material';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 export const AuthPage = () => {
-   return (
-      <div>
-         <h1>AuthPage test3</h1>
-      </div>
+   return (      
+         <>
+            <Box
+            sx={{ display: 'flex',
+          alignItems: 'center',}}
+            >
+               <div>
+                  <TextField
+                     id="input-with-icon-textfield"
+                     label="Username"
+                     variant="outlined"
+                     sx={{
+                        m:1,
+                        height:100
+                     }}
+                     InputProps={{
+                        endAdornment: (
+                           <InputAdornment position="end">
+                              <IconButton
+                              >
+                                 <AccountCircle />
+                              </IconButton>
+                           </InputAdornment>
+                        ),
+                     }}
+                  />
+               </div>
+               <div>
+                  <TextField
+                     id="input-with-icon-textfield"
+                     label="Password"
+                     variant="outlined"
+                     type='password'
+                     sx={{
+                        m:1,
+                        height:100
+                     }}
+                     InputProps={{
+                        endAdornment: (
+                           <InputAdornment position="end">
+                              <IconButton
+                              >
+                                 <Visibility />
+                              </IconButton>
+                           </InputAdornment>
+                        ),
+                     }}
+                  />
+               </div>
+            </Box>  
+         </>    
    )
 }

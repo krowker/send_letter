6e5c40fff3ed7feed8a8832c2f9a796c551f849c
krowker <krowker97@gmail.com>
2022-01-01 16:59:07 +0300

    add buttons, fix css issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,20 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
-import {useRoutes} from './routes'
+import { useRoutes } from './routes'
 import Container from '@mui/material/Container'
 
 function App() {
   const routes = useRoutes(false)
 
-  return (    
-    <Container 
-    maxWidth="sm"
-    sx={{ alignContent: 'center',
-    alignItems: 'center',
-    height: 100 }}
+  return (
+    <Container fixed
+      sx={{
+        height: '100vh'
+      }}
     >
-    <BrowserRouter>
+      <BrowserRouter>
         {routes}
-    </BrowserRouter>
+      </BrowserRouter>
     </Container>
   );
 }
--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -7,60 +7,90 @@ import OutlinedInput from '@mui/material/OutlinedInput'
 import InputAdornment from '@mui/material/InputAdornment'
 import AccountCircle from '@mui/icons-material/AccountCircle'
 import { IconButton } from '@mui/material';
+import Button from '@mui/material/Button'
+import Link from '@mui/material/Link'
+import Divider from '@mui/material/Divider'
+import Chip from '@mui/material/Chip'
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 export const AuthPage = () => {
-   return (      
-         <>
-            <Box
-            sx={{ display: 'flex',
-          alignItems: 'center',}}
+   return (
+      <>
+         <Box
+            sx={{
+               height: '100vh',
+               display: 'flex',
+               justifyContent: 'center',
+               alignItems: 'center',
+               flexDirection: 'column'
+            }}
+         >
+            <TextField
+               id="input-with-icon-textfield"
+               label="Username"
+               variant="outlined"
+               sx={{
+                  m: 1,
+               }}
+               InputProps={{
+                  endAdornment: (
+                     <InputAdornment position="end">
+                        <AccountCircle
+                           sx={{
+                              padding: '8px'
+                           }}
+                        />
+                     </InputAdornment>
+                  ),
+               }}
+            />
+            <TextField
+               id="input-with-icon-textfield"
+               label="Password"
+               variant="outlined"
+               type='password'
+               sx={{
+                  m: 1
+               }}
+               InputProps={{
+                  endAdornment: (
+                     <InputAdornment position="end">
+                        <IconButton
+                        >
+                           <Visibility />
+                        </IconButton>
+                     </InputAdornment>
+                  ),
+               }}
+            />
+            <Button
+               variant="contained"
+               sx={{
+                  m: 1,
+               }}
+            >Log In</Button>
+            <Link href='#'
+               underline="none"
+               sx={{
+                  p: 1
+               }}
+            >Forgot password?</Link>
+            {/* <Divider
+               variant='middle'
+               sx={{
+                  m: 1
+               }}
             >
-               <div>
-                  <TextField
-                     id="input-with-icon-textfield"
-                     label="Username"
-                     variant="outlined"
-                     sx={{
-                        m:1,
-                        height:100
-                     }}
-                     InputProps={{
-                        endAdornment: (
-                           <InputAdornment position="end">
-                              <IconButton
-                              >
-                                 <AccountCircle />
-                              </IconButton>
-                           </InputAdornment>
-                        ),
-                     }}
-                  />
-               </div>
-               <div>
-                  <TextField
-                     id="input-with-icon-textfield"
-                     label="Password"
-                     variant="outlined"
-                     type='password'
-                     sx={{
-                        m:1,
-                        height:100
-                     }}
-                     InputProps={{
-                        endAdornment: (
-                           <InputAdornment position="end">
-                              <IconButton
-                              >
-                                 <Visibility />
-                              </IconButton>
-                           </InputAdornment>
-                        ),
-                     }}
-                  />
-               </div>
-            </Box>  
-         </>    
+               <Chip label="or"/>
+            </Divider> */}
+            <Button
+               variant='outlined'
+               sx={{
+                  m: 3
+               }}
+            >Registration</Button>
+         </Box>
+      </>
    )
 }

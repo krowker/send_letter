85248d604c77ba320c8a0490e8670e5b00e6dc7d
krowker <krowker97@gmail.com>
2022-01-06 19:22:14 +0300

    #fabAddresess add fabButton

--- client/src/pages/AddressesPage.js
+++ client/src/pages/AddressesPage.js
@@ -1,5 +1,9 @@
 import React from 'react'
-import { Grid } from '@mui/material'
+import {
+   Grid,
+   Fab
+} from '@mui/material'
+import AddIcon from '@mui/icons-material/Add'
 import { AddressCard } from '../components/AddressCard'
 
 export const AddressesPage = () => {
@@ -7,6 +11,17 @@ export const AddressesPage = () => {
       <Grid container spacing={1}>
          <AddressCard></AddressCard>
          <AddressCard></AddressCard>
+         <Fab
+            color='primary'
+            sx={{
+               position: 'fixed',
+               bottom: '65px',
+               right: '10px',
+            }}
+
+         >
+            <AddIcon />
+         </Fab>
       </Grid>
    )
 }

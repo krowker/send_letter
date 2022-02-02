b8aa4f6c53b692a529aabd7e38ccd673ce605a25
krowker <krowker97@gmail.com>
2022-02-03 00:08:04 +0300

    #server edit connnection to mongo

--- server/index.js
+++ server/index.js
@@ -1,26 +1,28 @@
 require('dotenv').config()
-const express = require ('express')
+const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
-const mongoose = require ('mongoose')
-const errorMiddleware = require('./middlewares/error-middleware') 
+const mongoose = require('mongoose')
+const errorMiddleware = require('./middlewares/error-middleware')
 
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
-// app.use(cors({
-//    credentials:true,
-//    origin: process.env.CLIENT_URL
-// }))
+app.use(cors({
+   credentials: true,
+   origin: process.env.CLIENT_URL
+}))
 app.use('/api', require('./routes/routes'))
 app.use(errorMiddleware)
 
 const PORT = process.env.PORT
 
-const start = async() => {
+const start = async () => {
    try {
-      await mongoose.connect (`${process.env.DB_URL}`, {
-         useNewUrlParser: true
+      await mongoose.connect(`${process.env.DB_URL}`, {
+         useNewUrlParser: true,
+         useUnifiedTopology: true,
+         family: 4,
       })
       console.log(mongoose.connection.readyState);
       app.listen(PORT, () => console.log(`App has been started on port ${PORT}`))
--- server/service/token-service.js
+++ server/service/token-service.js
@@ -2,6 +2,15 @@ const jwt = require('jsonwebtoken')
 const tokenModel = require ('../models/token-model')
 
 class TokenService {
+   generateTokens(payload) {
+      const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn:'30m'})
+      const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {expiresIn:'30d'})
+      return {
+         accessToken,
+         refreshToken
+      }
+   }
+
    validateAccessToken(token) {
       try {
          const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET)
@@ -18,16 +27,7 @@ class TokenService {
       } catch (error) {
          return null;
       }
-   }
-
-   generateTokens(payload) {
-      const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn:'30m'})
-      const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {expiresIn:'30d'})
-      return {
-         accessToken,
-         refreshToken
-      }
-   }
+   }   
 
    async saveToken(userId, refreshToken) {
       const tokenData = await tokenModel.findOne ({user: userId})

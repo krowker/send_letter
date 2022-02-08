462dc34428609f0e2846141531aff2739aac5c00
krowker <krowker97@gmail.com>
2022-02-08 14:08:04 +0300

    #server fix db bug, off send activationmail

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,12 +1,12 @@
 const {Schema, model} = require ('mongoose')
 
 const UserSchema = new Schema ({
-   login: {type: String, unique: true, required: true},
+   email: {type: String, unique: true, required: true},
    password: {type: String, required: true},
    isActivated: {type: Boolean,  default: false},
    activationLink: {type: String},
-   address: {type: String, required: true},
-   gender: {type: String, required: true},
+   address: {type: String},
+   gender: {type: String},
    contacts: [{type: Schema.Types.ObjectId, ref: 'Contact'}],
    notifications: [{type: Schema.Types.ObjectId, ref: 'Notification'}]
 })
